6813441942911826287765906121d9a336f8483d
SRISHTI-24 <srishtimsaxena2002@gmail.com>
2021-11-16 02:36:26 +0530

    q4

--- src/shapemain.java
+++ src/shapemain.java
@@ -1,2 +1,54 @@
+class shaped
+{
+    public void draw()
+    {
+        System.out.println("Drawing shape");
+    }
+    public void erase()
+    {
+        System.out.println("Erasing shape");
+    }
+}
+class circle extends shaped{
+    public void draw()
+    {
+        System.out.println("Drawing circle");
+    }
+    public void erase()
+    {
+        System.out.println("Erasing circle");
+    }
+}
+class Triangle extends shaped{
+    public void draw()
+    {
+        System.out.println("Drawing Triangle");
+    }
+    public void erase()
+    {
+        System.out.println("Erasing Triangle");
+    }
+}
+class Square extends shaped{
+    public void draw()
+    {
+        System.out.println("Drawing Square");
+    }
+    public void erase()
+    {
+        System.out.println("Erasing Square");
+    }
+}
 public class shapemain {
+    public static void main(String[] args) {
+        shaped c = new circle();
+        c.draw();
+        c.erase();
+        shaped t = new Triangle();
+        t.draw();
+        t.erase();
+        shaped s = new Square();
+        s.draw();
+        s.erase();
+    }
 }
